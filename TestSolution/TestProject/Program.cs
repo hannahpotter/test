b417215ca45a010118f1dc89536cd0fda162fec1
hannahpotter <potter.hannah92@gmail.com>
2016-08-31 11:06:26 -0600

    Make another line to the console

--- TestSolution/TestProject/Program.cs
+++ TestSolution/TestProject/Program.cs
@@ -11,6 +11,7 @@ namespace TestProject
         static void Main(string[] args)
         {
             Console.WriteLine("This is a change");
+            Console.WriteLine("More changes");
         }
     }
 }
